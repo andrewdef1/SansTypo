1fc9f9cdc158e542ee31093153267518e6d158de
Andrew Patrick de Fretes <andrewbelanda@gmail.com>
2019-07-05 05:13:45 +0700

    update
    update bg, pause game

--- main.lua
+++ main.lua
@@ -7,10 +7,9 @@ Timer = require 'lib/knife.timer'
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
-ALPHABET = "abcdefghijklmnopqrstuvwxyz'-."
+ALPHABET = "abcdefghijklmnopqrstuvwxyz'-./"
 
-local utf8 = require("utf8")
-local font = love.graphics.newFont('fonts/MRF Lemonberry Sans.otf', 64)
+local font = love.graphics.newFont('fonts/UbuntuCondensed-Regular.ttf', 64)
 
 local currentTime = 60
 local currentCharIndex = 1
@@ -24,8 +23,13 @@ local start = true
 local gameOver = false
 local cursor = false
 
+local background
+
 function love.load()
+   
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+    
+    background = love.graphics.newImage('WoodBackground.png')
 
     love.graphics.setFont(font)
 
@@ -45,9 +49,16 @@ function love.load()
 
     initializeDictionary()
     chooseWord()
+    
+    Timer.game_paused = false
 end
 
-function love.keypressed(key)
+
+
+function love.keypressed(key, unicode)
+  
+ if key == 'return' then game_paused = not game_paused end
+
     if key == 'escape' then
         love.event.quit()
     end
@@ -60,6 +71,7 @@ function love.keypressed(key)
         gameOver = false
         score = 0
         chooseWord()
+        
     end
         
 
@@ -92,14 +104,34 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
-
-    if not start and not gameOver then
+   
+    if not start and not gameOver and not game_paused then
         Timer.update(dt)
     end
+    
+
 end
 
 function love.draw()
+  
+if game_paused then 
+  local sx = love.graphics.getWidth() / background:getWidth()
+  local sy = love.graphics.getHeight() / background:getHeight()
+  love.graphics.draw(background, 0, 0, 0, sx, sy) -- x: 0, y: 0, rot: 0, scale x and scale y
+  
+  love.graphics.setColor(1, 1, 0, 1)
+  if math.floor(love.timer.getTime()) % 2 == 0 then
+  love.graphics.print("GAME DI PAUSE" ,WINDOW_WIDTH / 2 - font:getWidth("GAME DI PAUSE") / 2, WINDOW_HEIGHT / 2 - 32)
+end
+love.graphics.setColor(1, 1, 1, 1)
+  return 
+  end
 
+  --background overall
+  local sx = love.graphics.getWidth() / background:getWidth()
+  local sy = love.graphics.getHeight() / background:getHeight()
+  love.graphics.draw(background, 0, 0, 0, sx, sy) -- x: 0, y: 0, rot: 0, scale x and scale y
+  
     -- draw the current goal word in yellow
     love.graphics.setColor(1, 1, 0, 1)
     love.graphics.print(fullString, WINDOW_WIDTH / 2 - font:getWidth(fullString) / 2, WINDOW_HEIGHT / 2 - 32)
@@ -120,24 +152,28 @@ function love.draw()
     -- draw the score in the top-right
     love.graphics.printf(tostring(score), 0, 0, WINDOW_WIDTH, 'right')
 
-    love.graphics.printf(tostring(#words) .. ' words loaded!',
+    love.graphics.printf(tostring(#words) .. ' kata telah dimuat!',
         0, 64, WINDOW_WIDTH, 'center')
 
     -- draw starting panel
     if start then
-        love.graphics.setColor(0.5, 0.5, 0.5, 1)
+        love.graphics.setColor(0.43, 0.28, 0.01, 1)
         love.graphics.rectangle('fill', 128, 128, WINDOW_WIDTH - 256, WINDOW_HEIGHT - 256)
         love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.printf('Press Space to Start', 0, WINDOW_HEIGHT / 2 - 32, WINDOW_WIDTH, 'center')
+        love.graphics.printf('Tekan SPACE untuk Mulai', 0, WINDOW_HEIGHT / 2 - 110, WINDOW_WIDTH, 'center')
+        love.graphics.printf('Tekan ENTER untuk Pause', 0, WINDOW_HEIGHT / 2 - 55, WINDOW_WIDTH, 'center')
+        love.graphics.printf('Tekan esc untuk Keluar', 0, WINDOW_HEIGHT / 2 - 1, WINDOW_WIDTH, 'center')
+        
     end
 
     if gameOver then
-        love.graphics.setColor(0.5, 0.5, 0.5, 1)
+        love.graphics.setColor(0.43, 0.28, 0.01, 1)
         love.graphics.rectangle('fill', 128, 128, WINDOW_WIDTH - 256, WINDOW_HEIGHT - 256)
         love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.printf('Game Over', 0, WINDOW_HEIGHT / 2 - 32, WINDOW_WIDTH, 'center')
-        love.graphics.printf('Your Score: ' .. tostring(score), 0, WINDOW_HEIGHT / 2 + 18, WINDOW_WIDTH, 'center')
-        love.graphics.printf('Press Space to Restart', 0, WINDOW_HEIGHT / 2 + 64, WINDOW_WIDTH, 'center')
+        love.graphics.printf('END GAME', 0, WINDOW_HEIGHT / 3 - 20, WINDOW_WIDTH, 'center')
+        love.graphics.printf('Skor kamu: ' .. tostring(score), 0, WINDOW_HEIGHT / 2 - 70, WINDOW_WIDTH, 'center')
+        love.graphics.printf('Tekan SPACE untuk Ngulang', 0, WINDOW_HEIGHT / 2 - 5, WINDOW_WIDTH, 'center')
+
     end
 end
 
